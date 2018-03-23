eec81bde9adec1f4ec773283089d6f3460b6dfff
Jonathan M. Tran <jmtran@csu.fullerton.edu>
2018-03-23 02:23:02 -0700

    Added Gradle task to start HSQLDB

--- docs/hsqlStart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -n "$1" ]; then
-  echo "Usage: $0 <classpath to hsqldb-*.jar>"
-  exit 1
-fi
-
-java -cp $1 org.hsqldb.server.Server -database.0 'file:./.gradle/hsqldb/projectX' --dbname.0 projectX
