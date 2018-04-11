305c4c214a1f8f32fa11bd0853b3335012b9cfd1
Jonathan M. Tran <jmtran@csu.fullerton.edu>
2018-04-11 14:55:16 -0700

    Cleaned up imports

--- src/main/java/edu/fullerton/csu/jmtran/projectx/messaging/service/DatabaseService.java
+++ src/main/java/edu/fullerton/csu/jmtran/projectx/messaging/service/DatabaseService.java
@@ -2,9 +2,6 @@ package edu.fullerton.csu.jmtran.projectx.messaging.service;
 
 import edu.fullerton.csu.jmtran.projectx.model.Message;
 import edu.fullerton.csu.jmtran.projectx.model.User;
-import edu.fullerton.csu.jmtran.projectx.service.HibernateMessageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 public class DatabaseService extends AbstractMessagingService {
     @Override
